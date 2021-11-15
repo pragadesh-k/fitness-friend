5484609b29d079d589855da4a9b41bed97e57a32
pragadeshk <pragadeshk31@gmail.com>
2021-11-15 17:03:47 +0530

    change favicon

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,22 @@
 import { Col, Navbar, NavbarBrand } from "react-bootstrap";
 import { SiDeepnote } from "react-icons/si";
+import { BsFillCloudSunFill } from "react-icons/bs";
 
 const Header = (props) => {
   return (
     <Navbar id="header" className="p-0 align-items-stretch">
       <NavbarBrand className="col-sm-3 col-lg-2 mr-0 p-4" id="left-face">
-        <SiDeepnote color="#2cbf6a" /> Fitness Friend
+        {/* <SiDeepnote color="#2cbf6a" /> Fitness Friend */}
       </NavbarBrand>
-      {/* <Col className="p-4 d-flex" id="right-face">
+      <Col className="p-4 d-flex" id="right-face">
         <div id="project-title" className="mx-auto">
           <SiDeepnote
             color="#2cbf6a"
-            style={{ marginRight: "1rem", verticalAlign: "middle" }}
+            style={{ marginRight: "1rem", verticalAlign: "text-top" }}
           />
-          fitness friend
+          <span>fitness friend</span>
         </div>
-      </Col> */}
+      </Col>
     </Navbar>
   );
 };
